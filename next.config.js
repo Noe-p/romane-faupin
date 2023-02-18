4bb6544f652f476a5c270f0231bbfd4aa137dbfc
Noé <noe.philippe@bob-le-developpeur.com>
2023-02-18 01:15:51 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 const { i18n } = require('./next-i18next.config');
-const runtimeCaching = require('next-pwa/cache');
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-});
-
-module.exports = withPWA({
+module.exports = {
   i18n,
   // distDir: 'build',
-  staticPageGenerationTimeout: 20000,
-});
+};

4e3b7076e451a48a71943bcd7dd448903ab4078b
Noé <noe.philippe@bob-le-developpeur.com>
2023-02-21 11:40:01 +0100

    ✨ add meta description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,12 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
             rel='stylesheet'
           />
+          <title>{'Romane Faupin'}</title>
+          <meta name='theme-color' content='white' />
+          <meta
+            name='description'
+            content="Née en 1999, Romane FAUPIN vit et travaille à Paris. Après un bac ES en candidat libre et des études technique de photographie, elle s'oriente vers une production plasticienne."
+          />
         </Head>
         <body>
           <Main />
